304fa08792b04ec2d0f5c1b70d323a2e754085c9
v028 <028@bms24.ru>
2017-07-30 06:41:55 +0300

    Initial commit

---
+++ src/test/TestClass.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by User on 10.06.2017.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+}
